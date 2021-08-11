cd4e03cd81a77ea7e380ffd4fc72e3817eabcbfe
byIx <40878600+byIx@users.noreply.github.com>
2021-08-12 02:33:41 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.1.1", "longversion":11001, "released":1628524115, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2490-click-to-mention\/" }
+{ "version":"1.2.0", "longversion":12000, "released":1628713833, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2490-click-to-mention\/" }
